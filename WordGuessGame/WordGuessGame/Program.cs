ede72f1747d25ad092d00e52719ee7923dbae6fc
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-03-21 14:11:52 -0700

    Wrote method to get a word from a file.

--- WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -6,7 +6,29 @@ namespace WordGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+
+        /// <summary>
+        /// Reads in the file of words and gets a single word returning it. If the file fails to read it writes the error to the console and just returns the word "Surprise"
+        /// </summary>
+        /// <param name="path">The path of the file to be read.</param>
+        /// <returns>Returns the word to be used by the game.</returns>
+        static string GetWord(string path)
+        {
+            try
+            {
+            string[] words = System.IO.File.ReadAllLines(path);
+            Random random = new Random();
+            int wordNumber = random.Next(0, words.Length);
+            return words[wordNumber];
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return "Surprise";
+            }
         }
     }
 }
